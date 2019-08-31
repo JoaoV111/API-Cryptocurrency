34fe9240446ac9c8a1e90de37f7ee60dd9cb2d67
JoaoV111 <52671234+JoaoV111@users.noreply.github.com>
2019-08-30 22:46:08 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# API-Cryptocurrency
-Return in json the conversion value between cryptocurrencies
+# Trilha DevOps
+
+<!-- Altere a Flag abaixo com sua URL do Travis -->
+[![Build Status](https://travis-ci.org/JoaoV111/API-Distances-Google.svg?branch=master)](https://travis-ci.org/JoaoV111/API-Distances-Google)
+
+## Aplicação criada para aprender a trabalhar com API no Flask
+
+
--- app.py
+++ app.py
@@ -1,37 +1,49 @@
-import flask
-from flask import request, jsonify
+from flask import request, jsonify, Flask
 from pycoingecko import CoinGeckoAPI
 
 cg = CoinGeckoAPI()
-
-
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+	return '''<h1>API-Cryptocurrencies Prices</h1>
+<p>Made by João Cesar</p>'''
 
-@app.route('/music-lyrics/', methods=['POST', 'GET'])
-def MusicLyrics():
-	lyrics = ''
-	Artist = 'artist'
-	Title = 'title'
-	if request.method == 'POST':
-		Artist = request.form['artist']
-		Title = request.form['title']
-		if Artist == '' or Title == '':
-			Artist = 'artist'
-			Title = 'title'
-		try:
-			url = f'https://api.lyrics.ovh/v1/{Artist}/{Title}'
-			req = requests.get(url)
-			lyrics_json = json.loads(req.text)
-			lyrics = lyrics_json["lyrics"]
-			lyrics = lyrics.replace("\n","<br>")
-		except:
-			lyrics = 'No lyrics found.'
-	
-	return render_template('musiclyrics.html', lyrics=lyrics, Artist=Artist, Title=Title)
+@app.route('/currency/all', methods=['GET'])
+def currencies_list():
+	return jsonify(cg.get_coins_list())
+
+@app.route('/currency', methods=['GET'])
+def currency_conv():
+	if 'f_id' in request.args:
+		f_id = str(request.args['f_id'])
+	else:
+		return "Error: No id field provided. Please specify the first id (f_id)."
+	if 's_id' in request.args:
+		s_id = str(request.args['s_id'])
+	else:
+		return "Error: No id field provided. Please specify the second id (s_id)."
+
+	try:
+		f_price = float(cg.get_price(ids = f_id, vs_currencies='usd')[f_id]['usd'])
+		s_price = float(cg.get_price(ids = s_id, vs_currencies='usd')[s_id]['usd'])
+		div1 = f_price/s_price
+		div2 = s_price/f_price
+		result = [{
+			'id' : f_id,
+			'price' : f_price,
+			'div' : div1
+		},
+		{
+			'id' : s_id,
+			'price' : s_price,
+			'div' : div2		
+		}]
+		return jsonify(result)
+	except:
+		return jsonify([{}])
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
